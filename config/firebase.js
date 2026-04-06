1288b8d65e3bd51512fe68b4a7c1eaf13b184a49
J0hnAr1as <johnarias@tutamil.com>
2026-04-05 21:40:26 -0500

    Correccion al archivo firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const admin = require("firebase-admin");
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
@@ -9,4 +10,5 @@ admin.initializeApp({
 
 const db = admin.firestore();
 module.exports = db;
+
 
